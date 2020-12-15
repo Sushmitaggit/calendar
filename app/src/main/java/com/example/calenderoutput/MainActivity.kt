f254453ca15121bdf50aed33f00073d610daa36a
sushmitaggit <susshmita101@yahoo.com>
2020-12-15 15:40:25 +0545

    MainActivity.kt

--- app/src/main/java/com/example/calenderoutput/MainActivity.kt
+++ app/src/main/java/com/example/calenderoutput/MainActivity.kt
@@ -11,13 +11,13 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var etDate : EditText
-    private lateinit var etDOB : EditText
+    private lateinit var etDate: EditText
+    private lateinit var etDOB: EditText
 
-    private lateinit var tvOut : TextView
+    private lateinit var tvOut: TextView
 
-    private lateinit var tvYear : TextView
-    private lateinit var tvAge : TextView
+    private lateinit var tvYear: TextView
+    private lateinit var tvAge: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,65 +31,73 @@ class MainActivity : AppCompatActivity() {
         tvYear = findViewById(R.id.tvYear)
         tvAge = findViewById(R.id.tvAge)
 
-        etDate.setOnClickListener(){
+        etDate.setOnClickListener() {
             loadCalendar()
         }
 
-        etDOB.setOnClickListener(){
+        etDOB.setOnClickListener() {
             loadCalender()
         }
     }
 
-    private fun loadCalendar(){
+    private fun loadCalendar() {
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-        val date = DatePickerDialog( this,
+        val date = DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     etDate.setText("  $year-${monthOfYear + 1}-$dayOfMonth")
 
-                    val dateJoined = LocalDateTime.of(year,monthOfYear+1,dayOfMonth,0,0)
-                    val current =  LocalDateTime.now()
-                    val diff = ChronoUnit.DAYS.between(dateJoined,current).toInt()
+                    val dateJoined = LocalDateTime.of(year, monthOfYear + 1, dayOfMonth, 0, 0)
+                    val current = LocalDateTime.now()
+                    val diff = ChronoUnit.DAYS.between(dateJoined, current).toInt()
 
                     var y = diff / 365
                     val d = diff - 365 * y
-                    var m = d/30
+                    var m = d / 30
 
-                    if(y == 0) { tvYear.text = " No. of years worked : $m month "  }
-                    else if(m == 0) { tvYear.text = " No. of years worked :  $y year "  }
-                    else { tvYear.text = " No. of years worked : $y year $m month "}
+                    if (y == 0) {
+                        tvYear.text = " No. of years worked : $m month "
+                    } else if (m == 0) {
+                        tvYear.text = " No. of years worked :  $y year "
+                    } else {
+                        tvYear.text = " No. of years worked : $y year $m month "
+                    }
                 },
-        year,
-        month,
-        day)
+                year,
+                month,
+                day)
         date.show()
 
     }
 
-    private fun loadCalender(){
+    private fun loadCalender() {
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-        val date = DatePickerDialog( this,
+        val date = DatePickerDialog(this,
                 DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     etDOB.setText("  $year-${monthOfYear + 1}-$dayOfMonth")
 
-                    val dob = LocalDateTime.of(year,monthOfYear+1,dayOfMonth,0,0)
-                    val current =  LocalDateTime.now()
-                    val diff = ChronoUnit.DAYS.between(dob,current).toInt()
+                    val dob = LocalDateTime.of(year, monthOfYear + 1, dayOfMonth, 0, 0)
+                    val current = LocalDateTime.now()
+                    val diff = ChronoUnit.DAYS.between(dob, current).toInt()
 
                     var y = diff / 365
                     val d = diff - 365 * y
-                    var m = d/30
+                    var m = d / 30
 
-                    if(y == 0) { tvAge.text = " Your Age is : $m month "  }
-                    else if(m == 0) { tvAge.text = " Your Age is :  $y year "  }
-                    else { tvAge.text = " Your Age is : $y year $m month "}
+                    if (y == 0) {
+                        tvAge.text = " Your Age is : $m month "
+                    } else if (m == 0) {
+                        tvAge.text = " Your Age is :  $y year "
+                    } else {
+                        tvAge.text = " Your Age is : $y year $m month "
+                    }
                 },
                 year,
                 month,
@@ -97,5 +105,3 @@ class MainActivity : AppCompatActivity() {
         date.show()
     }
 }
-
- ")
